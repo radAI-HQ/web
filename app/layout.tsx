4dbcf3f3af2825f963fbc2e3b5c31ea58445b241
Femi Obadimu <ficazzosam@gmail.com>
2024-02-22 12:46:14 +0100

    ready

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,10 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "RadAI - AI Doctor for Radiologists",
-  description: " RadAI is an AI Doctor for Radiologists. It is a gateway to instant radiology insights. It is your AI Doctor Companion.",
+  description: " RadAI is an AI Doctor for Radiologists. It is a gateway to instant radiology insights and AI-assisted radiology diagnosis app in Africa, It is your AI Doctor Companion.",
   icons: {
-    icon: '/favicon.ico',
+    icon: 'https://ik.imagekit.io/ubdvpx7xd0j/Radai/Svg%20Hash%20(2)_nt_EDDYiS.png?updatedAt=1708601573467',
+    apple: 'https://ik.imagekit.io/ubdvpx7xd0j/Radai/Svg%20Hash%20(2)_nt_EDDYiS.png?updatedAt=1708601573467',
   },
 };
 
@@ -17,11 +18,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-      </head>
       <body>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="./favicon.ico" sizes="any" />
         {children}
       </body>
     </html>
